858cd8f6d6f021100bebcf3044b18ce8eadd394a
Daniel Kratz <git@danielkratz.com>
2017-10-20 10:06:12 +0200

    Add german translation

---
+++ nls/de/strings.js
@@ -0,0 +1,8 @@
+// German
+
+define({
+    AppTitle: 'JavaScript Minimierer',
+    editMenuText: 'Erstelle minimierte JavaScript Version',
+    editMenuTextToggle: 'JavaScript Minimierung beim Speichern',
+    contextMenuText: 'Erstelle minimierte Version'
+});
